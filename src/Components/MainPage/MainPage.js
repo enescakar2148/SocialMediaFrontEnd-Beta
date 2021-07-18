9cb3a05694946d82bdcc04f87fcc904200e9454b
Enes Çakar <e.52nes@hotmail.com>
2021-07-18 22:51:18 +0300

    post display is succes

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react'
-import "./MainPage.css"
-import Post from '../Post/Post'
-import axios from 'axios';
+import React, { Component } from "react";
+import "./MainPage.css";
+import Post from "../Post/Post";
+import axios from "axios";
 
 export default class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postDatas: [],
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            postDatas: []
-        }
-    }
+  componentDidMount = () => {
+    let data;
 
-    componentDidMount = () => {
-        let data
-        axios.get("localhost:8080/api/posts").then((resp)=>{
-            data = resp
+    axios
+      .get("http://localhost:8080/api/posts")
+      .then((resp) => {
+        data = resp.data;
+        this.setState({
+            postDatas: data,
+        });
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
 
-            this.setState({
-                postDatas: data
-            })
-        }).catch((err)=>{
-            alert("Bir hata meydana geldi")
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                {
-                    this.state.postDatas.map((data) => {
-                        return (
-                            <Post data={data} key={data.id}/>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.state.postDatas.map((data) => {
+          return <Post data={data} key={data.id} />;
+        })}
+      </div>
+    );
+  }
 }
