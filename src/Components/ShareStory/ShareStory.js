d08b9c630dcf3f627973a22b3516a8135f451db6
devberk <devberkbusiness@outlook.com>
2021-07-19 21:14:25 +0300

    metodları bağlama

--- src/Components/ShareStory/ShareStory.js
+++ src/Components/ShareStory/ShareStory.js
@@ -59,14 +59,14 @@ export default class ShareStory extends Component {
                 <div className="sharePostCard">
                     <div className="sharePostText">Share Stroy</div>
                     <div className="sharePostInputs">
-                        <input type="file" placeholder="Photo" className="shareFileInput"/>
+                        <input type="file" placeholder="Photo" className="shareFileInput" onChange={this.photoInputHandler}/>
                     </div>
                     <div className="sharePostImgDiv">
                         <input type="text" placeholder="img link" className="tempInput" />
                         <img className="sharePostImg" width="150px" height="150px" alt=""/>
                     </div>
                     <div style={{textAlign:'center'}}>
-                        <button className="sharePostShareButton" >Share</button>
+                        <button className="sharePostShareButton" onClick={this.shareStoryButton} >Share</button>
                     </div>
                 </div>
             </div>
