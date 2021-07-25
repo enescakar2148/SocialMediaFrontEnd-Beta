ad9dfa7574dc6150382f8a5711c5fc084624d757
Enes Çakar <e.52nes@hotmail.com>
2021-07-25 17:42:18 +0300

    post arayüz bitt

--- src/Components/FeedSubComponents/Posts.js
+++ src/Components/FeedSubComponents/Posts.js
@@ -10,45 +10,213 @@ export default function Posts() {
     <div class="posts-wrapper">
       <div class="user-info-wrapper">
         <img src={userPp} id="pp-post" alt="Profile Image" />
-        <p id="status-user-name" class="status">User Name</p>
+        <p id="status-user-name" class="status">
+          User Name
+        </p>
       </div>
       <div class="post-image">
         <img src={post} id="post-image" alt="Post Image" />
       </div>
       <div class="options-wrapper">
-        <p id="status-user-name" class="status">User Name</p>
+        <div class="actions">
+            <ul class="action-icons">
+                <li class="action-icon"><i class="far fa-heart"></i></li>
+                <li class="action-icon"><i class="far fa-comment"></i></li>
+                <li class="action-icon"><i class="fas fa-share"></i></li>
+                <li class="action-icon"><i class="far fa-bookmark"></i></li>
+            </ul>
+        </div>
+        <div class="texts">
+          <p id="option-user-name" class="status">
+            User Name
+          </p>
+          <p id="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
       </div>
-
-      {/* 
+      
       <div class="user-info-wrapper">
         <img src={userPp} id="pp-post" alt="Profile Image" />
-        </div>
+        <p id="status-user-name" class="status">
+          User Name
+        </p>
+      </div>
       <div class="post-image">
         <img src={post} id="post-image" alt="Post Image" />
       </div>
-
-
+      <div class="options-wrapper">
+        <div class="actions">
+            <ul class="action-icons">
+                <li class="action-icon"><i class="far fa-heart"></i></li>
+                <li class="action-icon"><i class="far fa-comment"></i></li>
+                <li class="action-icon"><i class="fas fa-share"></i></li>
+                <li class="action-icon"><i class="far fa-bookmark"></i></li>
+            </ul>
+        </div>
+        <div class="texts">
+          <p id="option-user-name" class="status">
+            User Name
+          </p>
+          <p id="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+      </div>
+      
       <div class="user-info-wrapper">
         <img src={userPp} id="pp-post" alt="Profile Image" />
-        </div>
+        <p id="status-user-name" class="status">
+          User Name
+        </p>
+      </div>
       <div class="post-image">
         <img src={post} id="post-image" alt="Post Image" />
       </div>
-
-
+      <div class="options-wrapper">
+        <div class="actions">
+            <ul class="action-icons">
+                <li class="action-icon"><i class="far fa-heart"></i></li>
+                <li class="action-icon"><i class="far fa-comment"></i></li>
+                <li class="action-icon"><i class="fas fa-share"></i></li>
+                <li class="action-icon"><i class="far fa-bookmark"></i></li>
+            </ul>
+        </div>
+        <div class="texts">
+          <p id="option-user-name" class="status">
+            User Name
+          </p>
+          <p id="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+      </div>
+      
       <div class="user-info-wrapper">
         <img src={userPp} id="pp-post" alt="Profile Image" />
-        </div>
+        <p id="status-user-name" class="status">
+          User Name
+        </p>
+      </div>
       <div class="post-image">
         <img src={post} id="post-image" alt="Post Image" />
       </div>
-
+      <div class="options-wrapper">
+        <div class="actions">
+            <ul class="action-icons">
+                <li class="action-icon"><i class="far fa-heart"></i></li>
+                <li class="action-icon"><i class="far fa-comment"></i></li>
+                <li class="action-icon"><i class="fas fa-share"></i></li>
+                <li class="action-icon"><i class="far fa-bookmark"></i></li>
+            </ul>
+        </div>
+        <div class="texts">
+          <p id="option-user-name" class="status">
+            User Name
+          </p>
+          <p id="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+      </div>
+      
       <div class="user-info-wrapper">
         <img src={userPp} id="pp-post" alt="Profile Image" />
-        </div>
+        <p id="status-user-name" class="status">
+          User Name
+        </p>
+      </div>
       <div class="post-image">
         <img src={post} id="post-image" alt="Post Image" />
-      </div> */}
+      </div>
+      <div class="options-wrapper">
+        <div class="actions">
+            <ul class="action-icons">
+                <li class="action-icon"><i class="far fa-heart"></i></li>
+                <li class="action-icon"><i class="far fa-comment"></i></li>
+                <li class="action-icon"><i class="fas fa-share"></i></li>
+                <li class="action-icon"><i class="far fa-bookmark"></i></li>
+            </ul>
+        </div>
+        <div class="texts">
+          <p id="option-user-name" class="status">
+            User Name
+          </p>
+          <p id="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+      </div>
+      
+      <div class="user-info-wrapper">
+        <img src={userPp} id="pp-post" alt="Profile Image" />
+        <p id="status-user-name" class="status">
+          User Name
+        </p>
+      </div>
+      <div class="post-image">
+        <img src={post} id="post-image" alt="Post Image" />
+      </div>
+      <div class="options-wrapper">
+        <div class="actions">
+            <ul class="action-icons">
+                <li class="action-icon"><i class="far fa-heart"></i></li>
+                <li class="action-icon"><i class="far fa-comment"></i></li>
+                <li class="action-icon"><i class="fas fa-share"></i></li>
+                <li class="action-icon"><i class="far fa-bookmark"></i></li>
+            </ul>
+        </div>
+        <div class="texts">
+          <p id="option-user-name" class="status">
+            User Name
+          </p>
+          <p id="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+      </div>
+      
+      <div class="user-info-wrapper">
+        <img src={userPp} id="pp-post" alt="Profile Image" />
+        <p id="status-user-name" class="status">
+          User Name
+        </p>
+      </div>
+      <div class="post-image">
+        <img src={post} id="post-image" alt="Post Image" />
+      </div>
+      <div class="options-wrapper">
+        <div class="actions">
+            <ul class="action-icons">
+                <li class="action-icon"><i class="far fa-heart"></i></li>
+                <li class="action-icon"><i class="far fa-comment"></i></li>
+                <li class="action-icon"><i class="fas fa-share"></i></li>
+                <li class="action-icon"><i class="far fa-bookmark"></i></li>
+            </ul>
+        </div>
+        <div class="texts">
+          <p id="option-user-name" class="status">
+            User Name
+          </p>
+          <p id="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
